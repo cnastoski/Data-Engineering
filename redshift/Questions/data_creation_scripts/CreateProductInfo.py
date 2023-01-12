6629a2dd566b0899238e0dc403b94d91f615c09c
Chris <nastoski@msu.edu>
2023-01-11 21:15:30 -0500

    create script to make phone prices and create the csv file from that

---
+++ redshift/Questions/data_creation_scripts/CreateProductInfo.py
@@ -0,0 +1,24 @@
+import random
+
+product_id = ['iphone11', 'iphone12', 'iphone13', 'iphoneSE', 'IpadMax', 'IpadMini', 'laptop256', 'Macbook512',
+              'galaxy10', 'galaxy11', 'galaxy12', 'galaxy13', 'watch320', 'watch340', 'Nk320', 'Nk400', 'Nk500']
+
+manufacturing_price = ["{:.2f}".format(round(random.uniform(50.09, 300.09), 2)) for i in range(len(product_id))]
+
+
+def createManufacturingPrice(file_name: str):
+    input_file = open(file_name, 'w')
+    header = "product_id, manufacturing_price\n"
+    input_file.write(header)
+    for idx in range(len(product_id)):
+        row = f"{product_id[idx]}, {manufacturing_price[idx]}\n"
+        input_file.write(row)
+
+    input_file.close()
+    return 1
+
+def main():
+    createManufacturingPrice("manufacturing_price.csv")
+
+
+main()
